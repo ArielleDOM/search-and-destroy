41ab286ff9e31c8ecc9257d40174b6ec56d025c5
Arielle Domantay <domantayarielle@gmail.com>
2020-04-08 15:22:10 -0400

    Finished binary tree WIP extra credit for it

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,31 @@
 'use strict';
 
 // Complete this algo
+// [1, 7, 3] 3
 const binarySearch = (array, target) => {
-	
+	let point = 0
+
+	while(point < array.length){
+		if(array[point] === target){
+			return true
+		}else {
+			point++
+		}
+	}
+
+///FOR EXTRA Credit maybe use recusion?
+	// if(point < array.length){
+	// 	if(array[point] === target){
+	// 		return true
+	// 	}else{
+	// 		point++
+	// 		binarySearch(array, target)
+	// 	}
+	// }
+
+	return false
 };
+
 
 /*
 	EXTRA CREDIT:
