71a63ea7cbb45bd1e3d9cf89e40b731018c1b337
Arielle Domantay <domantayarielle@gmail.com>
2020-04-08 16:35:33 -0400

    Passing binary search WIP, extra credit

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,34 +2,36 @@
 
 // Complete this algo
 // [1, 7, 3] 3
-//[1, 2, 3 , 4 , 5 , 6 ]  5
+
+
+//[1, 2, 3 , 4 , 5 , 6 ]  8
+// [4, 5, 6], 8
+//[5,6] ,8
+// [6], 8
 const binarySearch = (array, target) => {
-  let point = 0;
+
   let middle = Math.floor(array.length / 2);
-  let resolved = false;
-  if(array.length <= 1){
-	  return false 
+
+  if(array.length === 1){
+	  if(array[0] === target){
+      return true
+    } else{
+      return false
+    }
   }
+
   if (array[middle] === target) {
     return true;
   }
+
   if (array[middle] < target) {
 	let arrayMiddle = array.slice(middle)
-	return binarySearch(arrayMiddle, target); //[5, 6], 5
+	return binarySearch(arrayMiddle, target);
   } else {
-	let arrayMiddle = array.slice(0, middle)
-    return binarySearch( arrayMiddle, target); //
+	  let arrayMiddle = array.slice(0, middle)
+    return binarySearch( arrayMiddle, target); 
   }
-  ///FOR EXTRA Credit maybe use recusion?
-  // if(point < array.length){
-  // 	if(array[point] === target){
-  // 		return true
-  // 	}else{
-  // 		point++
-  // 		binarySearch(array, target)
-  // }
 
-  
 };
 
 /*
